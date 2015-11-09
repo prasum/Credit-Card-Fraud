8685a14fa16f4270c6d43399517f30399b2863df
Sumeet Lalla <sumeetlalla1994_@bitbucket.org>
2015-11-10 02:09:17 +0530

    New fix

--- ShopMobile/MarkovChain/Markov.php
+++ ShopMobile/MarkovChain/Markov.php
@@ -2,7 +2,7 @@
 error_reporting(-1);
 ini_set('display_errors', true);
 class Markov {
-    public $arr0;
+    protected $arr0;
     protected $arr1;
     protected $arr2;
     public $n;
@@ -15,12 +15,19 @@ class Markov {
     public $p20;
     public $p21;
     public $p22;
-    public function __construct($p)
+    public $item;
+    protected $initial;
+    protected $final;
+
+    /**
+     *  Constructor $p
+     */
+    public function __construct()
     {
         $this->arr0=array(3);
         $this->arr1=array(3);
         $this->arr2=array(3);
-        $this->n=$p;
+        $this->n=0;
         $this->p00=0;
         $this->p01=0;
         $this->p02=0;
@@ -30,7 +37,9 @@ class Markov {
         $this->p20=0;
         $this->p21=0;
         $this->p22=0;
-
+        $this->item=0;
+        $this->initial=array();
+        $this->final=array();
         for($i=0; $i<3; $i++)
         {
             $this->arr0[$i]=0;
@@ -38,27 +47,78 @@ class Markov {
             $this->arr2[$i]=0;
         }
     }
-    public function calculate_constants($first=array(),$last=array())
+    public function add_item($p)
+    {
+        $this->item=$p;
+    }
+    public function convert()
     {
 
-
-        if($this->n>1)
+        if($this->item<10000)
         {
-            for($j=0; $j<count($first); $j++)
+            return 0;
+        }
+        elseif($this->item>=10000 && $this->item<20000)
+        {
+            return 1;
+
+        }
+        else
+        {
+            return 2;
+        }
+    }
+    public function add_state()
+    {
+    $s=$this->convert();
+    if($this->n==0)
+    {
+        array_push($this->initial,$s);
+    }
+        elseif($this->n>0 && $this->n<4)
+        {
+            array_push($this->initial,$s);
+            array_push($this->final,$s);
+        }
+        elseif($this->n==4)
+        {
+            array_push($this->final,$s);
+        }
+        $this->n+=1;
+    }
+    public function view_state()
+    {
+        var_dump($this->initial);
+        var_dump($this->final);
+    }
+
+    /**
+     Function evaluating transition probabilities
+     * @param array $first
+     * @param array $last
+     */
+    public function calculate_constants()
+    {
+        if($this->n==0) {
+            echo "<b>" . "Number of trials cannot be zero" . "</b>";
+            return;
+        }
+        else{
+
+            for($j=0; $j<count($this->initial); $j++)
             {
-                if($first[$j]>=0 && $first[$j]<3)
+                if($this->initial[$j]>=0 && $this->initial[$j]<3)
                 {
-                    if($last[$j]>=0 && $last[$j]<3)
+                    if($this->final[$j]>=0 && $this->final[$j]<3)
                     {
-                        $var = 'arr'.$first[$j];
-                        $this->{$var}[$last[$j]] += 1;
+                        $var = 'arr'.$this->initial[$j];
+                        $this->{$var}[$this->final[$j]] += 1;
 
                     }
                 }
             }
-        }
-        else
-        {
+
+
             for($k=0; $k<3; $k++)
             {
                 $var='p0'.$k;
@@ -77,13 +137,17 @@ class Markov {
                 $this->{$var2}=$this->arr2[$k]/(($this->arr2[0])+($this->arr2[1])+($this->arr2[2]));
             }
         }
-        $this->n-=1;
+
     }
+
+    /**
+     Function Displaying Transition Probabilities
+     */
     public function display_constants()
     {
-        print_r($this->arr0);
+       /* print_r($this->arr0);
         print_r($this->arr1);
-        print_r($this->arr2);
+        print_r($this->arr2); */
         echo '<br/>';
         echo '<p>'.'<em>'.'Value of Markov Chain Transition Probabilities are:'.'</em></p>';
         echo  $this->p00;
@@ -100,10 +164,10 @@ class Markov {
     }
 }
 
-$m= new Markov(4);
+/*$m= new Markov(4);
 $m->calculate_constants(array(0,1,2),array(1,1,1));
 $m->calculate_constants(array(0,1,2),array(0,0,0));
 $m->calculate_constants(array(1,0,2),array(1,0,1));
 $m->calculate_constants();
-$m->display_constants();
+$m->display_constants(); */
 ?>
--- ShopMobile/MarkovChain/test.php
+++ ShopMobile/MarkovChain/test.php
@@ -1,9 +1,29 @@
+
 <?php
+error_reporting(-1);
+ini_set('display_errors', true);
+
 require 'Markov.php';
+echo "<em>"."Tests"."</em>";
+echo "<br><br>";
 echo "<em>"."Test1"."</em>";
 echo "<br>";
-$m= new Markov(4);
-$m->calculate_constants(array(0,1,2),array(1,1,1));
+$m= new Markov();
+$m->add_item(6000);
+$m->add_state();
+$m->add_item(15000);
+$m->add_state();
+$m->add_item(21000);
+$m->add_state();
+$m->add_item(3000);
+$m->add_state();
+$m->add_item(25000);
+$m->add_state();
+$m->view_state();
+$m->calculate_constants();
+$m->display_constants();
+
+/*$m->calculate_constants(array(0,1,2),array(1,1,1));
 $m->calculate_constants(array(0,1,2),array(0,0,0));
 $m->calculate_constants(array(1,0,2),array(1,0,1));
 $m->calculate_constants();
@@ -15,6 +35,6 @@ $m= new Markov(0);
 echo "<br>";
 $m->calculate_constants(array(0,1,2),array(1,1,1));
 $m->display_constants();
-
+*/
 
 ?>
