2569f3462a6281f589bfe09574a07f4073b9f3d1
Sumeet Lalla <sumeetlalla1994_@bitbucket.org>
2015-11-11 03:09:11 +0530

    State Solver

--- ShopMobile/MarkovChain/Markov.php
+++ ShopMobile/MarkovChain/Markov.php
@@ -17,6 +17,15 @@ class Markov {
     public $p22;
     public $item;
     public $flag;
+    public $cal_done;
+    public $c0;
+    public $c1;
+    public $c2;
+    public $eq_form;
+    protected $ieq;
+    protected $x0;
+    protected $x1;
+    protected $x2;
     protected $initial;
     protected $final;
     protected $shop_cart;
@@ -27,9 +36,13 @@ class Markov {
      */
     public function __construct()
     {
+        $this->x0=array(3);
+        $this->x1=array(3);
+        $this->x2=array(3);
         $this->arr0=array(3);
         $this->arr1=array(3);
         $this->arr2=array(3);
+        $this->ieq=array(3);
         $this->n=0;
         $this->p00=0;
         $this->p01=0;
@@ -42,6 +55,11 @@ class Markov {
         $this->p22=0;
         $this->item=0;
         $this->flag=0;
+        $this->cal_done=0;
+        $this->eq_form=0;
+        $this->c0=-1;
+        $this->c1=-1;
+        $this->c2=-1;
         $this->initial=array();
         $this->final=array();
         $this->shop_cart=array();
@@ -50,20 +68,42 @@ class Markov {
             $this->arr0[$i]=0;
             $this->arr1[$i]=0;
             $this->arr2[$i]=0;
+            $this->x0[$i]=0;
+            $this->x1[$i]=0;
+            $this->x2[$i]=0;
+            $this->ieq[$i]=1;
         }
     }
-    public function get_transaction_no()
+
+    /**
+     * Get  number of items
+     * @return int
+     */
+    public function get_item_no()
     {
         return $this->n;
     }
+
+    /**
+     * get item price
+     * @return int
+     */
     public function get_item()
     {
         return $this->item;
     }
+
+    /**
+     * insert item in cart
+     */
     public function insert_in_cart()
     {
       array_push($this->shop_cart,$this->item);
     }
+
+    /**
+     * View cart
+     */
     public function view_cart()
     {
         echo "Shopping list"."<br/>";
@@ -74,10 +114,20 @@ class Markov {
             echo "<br/>";
         }
     }
+
+    /**
+     * Add item for conversion
+     * @param $p
+     */
     public function add_item($p)
     {
         $this->item=$p;
     }
+
+    /**
+     * Converts price to state
+     * @return int
+     */
     public function convert()
     {
 
@@ -95,6 +145,11 @@ class Markov {
             return 2;
         }
     }
+
+    /**
+     * add initial and final state
+     * @param int $f
+     */
     public function add_state($f=0)
     {
         $this->flag=$f;
@@ -114,6 +169,10 @@ class Markov {
         }
         $this->n+=1;
     }
+
+    /**
+     * View State
+     */
     public function view_state()
     {
         var_dump($this->initial);
@@ -165,7 +224,7 @@ class Markov {
                 $this->{$var2}=$this->arr2[$k]/(($this->arr2[0])+($this->arr2[1])+($this->arr2[2]));
             }
         }
-
+    $this->cal_done=1;
     }
 
     /**
@@ -189,6 +248,120 @@ class Markov {
         echo '&nbsp;'.'&nbsp;'.$this->p22;
 
 
+    }
+    public function calculate_prob_state()
+    {
+      if($this->flag==1 && $this->cal_done==1)
+      {
+          for($j=0; $j<3; $j++)
+          {
+            for($k=0; $k<3; $k++)
+            {
+                $var='p'.$k.$j;
+                $var2=$this->{$var};
+                $var1='x'.$j;
+                $this->{$var1}[$k]=$var2;
+            }
+          }
+          for($r=0; $r<3; $r++)
+          {
+              for($s=0; $s<3; $s++)
+              {
+
+                  $var1='x'.$r;
+                  $var3='c'.$r;
+                  $var4=$this->{$var3};
+                  if($r==$s)
+                  {
+                      $this->{$var1}[$s]+=$var4;
+                  }
+              }
+          }
+          $e=0;
+          $var1='x'.$e;
+          for($w=0; $w<3; $w++)
+          {
+              $this->{$var1}[$w]+=$this->ieq[$w];
+          }
+      }
+        /*var_dump($this->x0);
+        var_dump($this->x1);
+        var_dump($this->x2);
+
+*/
+    $this->eq_form=1;
+    }
+
+    public function swap_rows(&$a, &$b, $r1, $r2)
+    {
+        if ($r1 == $r2) return;
+
+        $tmp = $a[$r1];
+        $a[$r1] = $a[$r2];
+        $a[$r2] = $tmp;
+
+        $tmp = $b[$r1];
+        $b[$r1] = $b[$r2];
+        $b[$r2] = $tmp;
+    }
+
+    public function gauss_eliminate($A, $b, $N)
+    {
+        for ($col = 0; $col < $N; $col++)
+        {
+            $j = $col;
+            $max = $A[$j][$j];
+
+            for ($i = $col + 1; $i < $N; $i++)
+            {
+                $tmp = abs($A[$i][$col]);
+                if ($tmp > $max)
+                {
+                    $j = $i;
+                    $max = $tmp;
+                }
+            }
+
+            $this->swap_rows($A, $b, $col, $j);
+
+            for ($i = $col + 1; $i < $N; $i++)
+            {
+                $tmp = $A[$i][$col] / $A[$col][$col];
+                for ($j = $col + 1; $j < $N; $j++)
+                {
+                    $A[$i][$j] -= $tmp * $A[$col][$j];
+                }
+                $A[$i][$col] = 0;
+                $b[$i] -= $tmp * $b[$col];
+            }
+        }
+        $x = array();
+        for ($col = $N - 1; $col >= 0; $col--)
+        {
+            $tmp = $b[$col];
+            for ($j = $N - 1; $j > $col; $j--)
+            {
+                $tmp -= $x[$j] * $A[$col][$j];
+            }
+            $x[$col] = $tmp / $A[$col][$col];
+        }
+        return $x;
+    }
+    function test_gauss()
+    {
+        if ($this->eq_form == 1) {
+            $a = array(
+                $this->x0, $this->x1, $this->x2
+
+
+            );
+            $b = array(1, 0, 0);
+
+            $x = $this->gauss_eliminate($a, $b, 3);
+
+            ksort($x);
+            print_r($x);
+        }
     }
 }
 
--- ShopMobile/MarkovChain/test.php
+++ ShopMobile/MarkovChain/test.php
@@ -18,10 +18,21 @@ $m->add_state();
 $m->add_item(3000);
 $m->add_state();
 $m->add_item(25000);
-$m->add_state();
+$m->add_state(1);
 $m->view_state();
 $m->calculate_constants();
 $m->display_constants();
+$m->calculate_prob_state();
+$m->test_gauss();
+/*$t=serialize($m);
+
+$mysqli=new mysqli('localhost','root','password','transaction_store');
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$q=$mysqli->query("insert into object_store(object) VALUES('".$t."')");
 
 /*$m->calculate_constants(array(0,1,2),array(1,1,1));
 $m->calculate_constants(array(0,1,2),array(0,0,0));
